80c0cb9a848dfc2dcf091f6153e9516e7fdac36f
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-02 14:39:44 -0500

    setup book model and initialized four book objects

--- LabExercise7/Book.swift
+++ LabExercise7/Book.swift
@@ -5,3 +5,31 @@
 //  Created by Penny Ahlstrom on 2026-03-02.
 //
 
+import Foundation
+
+struct Book: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let author: String
+}
+
+extension Book {
+    static let sampleBooks: [Book] = [
+        .init(
+            title: "Silent Spring",
+            author: "Rachel Carson"
+        ),
+        .init(
+            title: "The Immortal Life of Henrietta Lacks",
+            author: "Rebecca Skloot"
+        ),
+        .init(
+            title: "The End of Everything (Astrophysically Speaking)",
+            author: "Katie Mack"
+        ),
+        .init(
+            title: "Braiding Sweetgrass",
+            author: "Robin Wall Kimmerer"
+        )
+    ]
+}
