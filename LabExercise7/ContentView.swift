059c8aaeeb6a82fb950f4d6e353903dade9b7e8e
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-02 13:03:23 -0500

    Initial Commit

---
+++ LabExercise7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LabExercise7
+//
+//  Created by Penny Ahlstrom on 2026-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LabExercise7/LabExercise7App.swift
@@ -0,0 +1,17 @@
+//
+//  LabExercise7App.swift
+//  LabExercise7
+//
+//  Created by Penny Ahlstrom on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct LabExercise7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
